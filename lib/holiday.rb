25690622accaf26ad941aa1db36f19626e1396b6
Marco Miranda <marmirand@hotmail.com>
2019-04-11 13:04:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   holiday_supplies[:spring][:marco_day] = ["beer", "BBQ", "Cake"]
+  holiday_supplies
 end
 
 def all_winter_holiday_supplies(holiday_hash)
